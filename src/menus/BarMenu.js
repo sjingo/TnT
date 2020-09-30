aa56f35cc7314054beabcfa3bbe478fc57a03a37
sjingoFOD <stefan.ingolfsson@fleetondemand.com>
2020-09-30 14:03:33 +0100

    menus

--- src/menus/BarMenu.js
+++ src/menus/BarMenu.js
@@ -9,32 +9,17 @@ export default function () {
         setModalOpen(true)
         setImageUrl(url)
     }
-    const cards = [
-        {
-            header: 'Beer, wine, fizz, cocktails',
-            description: 'Drinks Menu 1',
-            image: BarMenuImage,
-            imageLink: '/BarMenu_page_1.jpg',
-        },
-        {
-            header: 'Mixers, gins, spirits, rum, tequilas ... ',
-            description: 'Drinks Menu 2',
-            image: BarMenuImage,
-            imageLink: '/BarMenu_page_2.jpg',
-        },
-    ]
-    return (
+     return (
         <>
             <Modal
                 onClose={() => setModalOpen(false)}
                 onOpen={() => setModalOpen(true)}
                 open={modalOpen}
                 closeIcon
-                basic
-                size="fullscreen"
+                
             >
                 <Modal.Content image>
-                    <Image size="massive" src={imageUrl} wrapped />
+                    <Image size="large" src={imageUrl} style={{width: '100%'}}  />
                 </Modal.Content>
             </Modal>
             <Header as="h2" color="teal">
@@ -44,28 +29,26 @@ export default function () {
                     Food and Drink
                 </Header.Content>
             </Header>
-            <Card.Group itemsPerRow={2}>
-                {cards.map((item) => (
-                    <>
-                        <Card key={item.header}>
+            <Card.Group itemsPerRow={1}>
+                        <Card >
                             <Card.Content>
-                                <Card.Header>{item.header}</Card.Header>
+                                <Card.Header>Takk Drinks Menu</Card.Header>
                             </Card.Content>
                             <img
-                                src={item.image}
-                                alt={item.description}
+                                src={BarMenuImage}
+                                alt="Takk drinks menu"
                                 style={{ width: '100%' }}
                             />
                             <Card.Content>
                                 <Card.Description>
-                                    {item.description}
+                                Beer, wine, fizz, cocktails, ixers, gins, spirits, rum, tequilas ...
                                 </Card.Description>
                             </Card.Content>
                             <Card.Content extra>
                                 <Button
                                     positive
                                     onClick={() =>
-                                        handleSetOpen(item.imageLink)
+                                        handleSetOpen('/BarMenu.jpg')
                                     }
                                 >
                                     <Icon name="image outline" />
@@ -73,8 +56,6 @@ export default function () {
                                 </Button>
                             </Card.Content>
                         </Card>
-                    </>
-                ))}
             </Card.Group>
             <Card.Group itemsPerRow={1}>
                 <Card as="a">
